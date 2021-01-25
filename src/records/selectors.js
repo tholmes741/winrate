b715101da718853baf835a46b4159f79a5a2844b
Tommy Holmes <holmesiii.thomas@gmail.com>
2021-01-25 01:03:26 -0500

    add reselect package

--- src/records/selectors.js
+++ src/records/selectors.js
@@ -1,1 +1,15 @@
+import { createSelector } from 'reselect';
+
 export const getRecords = state => state.records.data;
+export const getRecordsFilter = state => state.records.filter;
+
+export const getGames = createSelector(
+    getRecords,
+    (records)  => {
+        const games = new Set();
+        for (let record of records) {
+            games.add(record.game);
+        }
+        return Array.from(games);
+    }
+)
